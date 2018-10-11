e59bd27737458fa56e7c6b760a412dc9f7c5a27b
kushinn <lf.kushinn@gmail.com>
2018-10-11 14:51:31 +0800

    Tutorial01

---
+++ Unity_ECS_Tutorials/Assets/UnityECSTutorials/Tutorial01/Runtime/Boostrap.cs
@@ -0,0 +1,22 @@
+﻿using Unity.Entities;
+using Unity.Rendering;
+using UnityEngine;
+
+namespace RafeTutorials
+{
+    public class Boostrap : MonoBehaviour
+    {
+        public Mesh playerMesh;
+        public Material playerMaterial;
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+        private void Start()
+        {
+            var entityManager = World.Active.GetOrCreateManager<EntityManager>();
+
+            EntitySpawnManager.CreatePlayerArchetype(entityManager);
+            EntitySpawnManager.CreatePlayer(entityManager, new MeshInstanceRenderer() { mesh = playerMesh, material = playerMaterial });
+        }
+    }
+}
+
